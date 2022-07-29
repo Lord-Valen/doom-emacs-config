49cc83740dfbc5b5b5b3a62c83a67e458d146d8e
Lord-Valen <lord_valen@protonmail.com>
2022-07-29 18:14:03 -0400

    :rocket: `feat` install git-ontology-snippets

--- packages.el
+++ packages.el
@@ -6,5 +6,7 @@
 ;; use 'M-x doom/reload'.
 (package! evil-colemak-basics)  ; Colemak remaps
 (package! exec-path-from-shell)
-(package! org-modern)
+(package! org-modern)           ; Fancy org-mode
 (package! elcord)               ; Discord RPC. After all, why not?
+(package! git-ontology-snippets ; Snippets for Michael Moreno's git commit ontology
+  :recipe (:host github :repo "Lord-Valen/git-ontology-snippets.el"))
